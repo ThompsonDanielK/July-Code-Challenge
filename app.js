22b270742e76303f11d6229305b3587bb4869b2d
Dan Thompson <dthompson@revlocal.com>
2022-07-06 01:32:41 -0400

    Added functionss to check for winning hands

--- app.js
+++ app.js
@@ -35,28 +35,71 @@ export default class App {
     }
 
     findWinningHands() {
-        if (this.spades.total > 4 && this.findConsecutiveRanks(this.spades) > 4) {
-            
+        if (this.straightFlushChecker()) {
+            return;
+        } else if (fourOfAKindChecker()) {
+            return;
+        } else if (fullHouseChecker()) {
+            return;
         }
     }
 
-    straightFlushChecker(suit) {
-        const numberOfConsecutiveRanks = this.findConsecutiveRanks(suit);
+    straightFlushChecker() {
+        let suits = [this.clubs, this.diamonds, this.hearts, this.spades];
+        suits.forEach(suit => {
+            if (suit.total > 4) {
+                const consecutiveRanks = this.findConsecutiveRanks(suit);
+                if (consecutiveRanks.length > 4) {
+                    this.printWinner("Straight-Flush", consecutiveRanks);                    
+                    return true;
+                }
+            }
+        });
+        return false;
+    }
+
+    fourOfAKindChecker() {
+        suits.forEach(suit => {
+            if (suit.total > 4) {
+                const consecutiveRanks = this.findConsecutiveRanks(suit);
+                this.printWinner("Four-Of-A-Kind", consecutiveRanks);
+                return true;
+            }
+        });
+        return false;
+    }
+
+    fullHouseChecker() {
+        let suits = [this.clubs, this.diamonds, this.hearts, this.spades];
+        let threeOfASuit = false;
+        let twoOfASuit = false;
+        suits.forEach(suit => {
+            if (suit.total = 3) {
+                threeOfASuit = true;
+            } else if (suit.total = 2) {
+                twoOfASuit = true;
+            }
+        });
+        if ( threeOfASuit && threeOfASuit) {
+            const consecutiveRanks = this.findConsecutiveRanks(suit);
+            this.printWinner("Full House", consecutiveRanks);
+            return true;               
+        }
+        return false;
     }
 
     findConsecutiveRanks(cards) {
-        let consecutiveRanks = 0;
+        let consecutiveRanks = [];
         let previousRank;        
-        let sortedRankList = convertRanksToSortedNumericalList(rankList);
-        for (let index = 0; index < sortedRankList.length; index++) {
-            const rank = sortedRankList[index];
+        let sortedNumericalRankList = convertRanksToSortedNumericalList(rankList);
+        for (let index = 0; index < sortedNumericalRankList.length; index++) {
+            const rank = sortedNumericalRankList[index];
             if (rank - 1 == previousRank) {
-                consecutiveRanks++;
-            } else {
-                consecutiveRanks = 0;
+                consecutiveRanks.push(rank);
             }
             previousRank = rank;            
         }
+        consecutiveRanks = this.convertRanksToSortedStringListWithoutDuplicates(consecutiveRanks);
         return consecutiveRanks;
     }
 
@@ -81,8 +124,39 @@ export default class App {
                 default:
                     rank = suitKey;
             }
-            rankList.add(rank);
+            rankList.push(rank);
         }
         return rankList.sort(function(a, b){return a - b});
+    }
+
+    convertRanksToSortedStringListWithoutDuplicates(cards) {
+        let rankList = [];
+        for (let index = 0; index < Object.keys(cards).length; index++) {
+            let rank;
+            let suitKey = Object.keys(cards)[index];
+            switch(suitKey) {
+                case 11:
+                    rank = "A";
+                    break;
+                case 12:
+                    rank = "J";
+                    break;
+                case 13:
+                    rank = "Q";
+                    break;
+                case 14:
+                    rank = "K";
+                    break;
+                default:
+                    rank = suitKey.toString();
+            }
+            rankList.push(rank);
+        }
+        let uniqueRankList = [...new Set(rankList)];
+        return uniqueRankList;
+    }
+
+    printWinner(type, consecutiveRanks) {
+        console.log({type:type,  ranks:consecutiveRanks});
     }
 }
