fb646ecbce748acc34420b5e90cd9e3ce1cd6546
Dan Thompson <dthompson@revlocal.com>
2022-07-12 23:57:58 -0400

    Refactored existing process to reuse existing logic

--- app.js
+++ app.js
@@ -1,113 +1,174 @@
-export default class App {
-    spades = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, "A":0, "J":0, "Q":0, "K":0, total:0};
-    hearts = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, "A":0, "J":0, "Q":0, "K":0, total:0};
-    clubs = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, "A":0, "J":0, "Q":0, "K":0, total:0};
-    diamonds = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, "A":0, "J":0, "Q":0, "K":0, total:0};
 
-    hand(holeCards, comCards) {
-        let allCards = holeCards.concat(comCards);
-        this.countCards(allCards);
-    }
+let spades = {};
+let hearts = {};
+let clubs = {};
+let diamonds = {};
 
-    countCards(cards) {
-        cards.forEach(card => {
-            const suit = card[card.length - 1];
-            const rank = card.substring(0, card.length - 2)
-            switch(suit) {
-                case "♦":
-                    diamonds[rank]++;
-                    diamonds[total]++;
-                    break;
-                case "♣":
-                    clubs[rank]++;
-                    clubs[total]++;
-                    break;
-                case "♥":
-                    hearts[rank]++;
-                    hearts[total]++;
-                    break;
-                case "♠":
-                    diamonds[rank]++;
-                    diamonds[total]++;
-                    break;
-            }
-        });
-    }
+function hand(holeCards, comCards) {
+    spades = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, "A":0, "J":0, "Q":0, "K":0, total:0, consecutiveRanks:[]};
+    hearts = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, "A":0, "J":0, "Q":0, "K":0, total:0, consecutiveRanks:[]};
+    clubs = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, "A":0, "J":0, "Q":0, "K":0, total:0, consecutiveRanks:[]};
+    diamonds = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, "A":0, "J":0, "Q":0, "K":0, total:0, consecutiveRanks:[]};
 
-    findWinningHands() {
-        if (this.straightFlushChecker()) {
-            return;
-        } else if (fourOfAKindChecker()) {
-            return;
-        } else if (fullHouseChecker()) {
-            return;
+    let allCards = holeCards.concat(comCards);
+    countCards(allCards);
+    let suits = [clubs, diamonds, hearts, spades];
+    suits.forEach(suit => {
+        suit.consecutiveRanks = findConsecutiveRanks(suit);          
+    });
+    findWinningHands();
+}
+
+function countCards(cards) {
+    cards.forEach(card => {
+        const suit = card[card.length - 1];
+        const rank = card.substring(0, card.length - 1);
+        switch(suit) {
+            case "♦":
+                diamonds[rank]++;
+                diamonds.total++;
+                break;
+            case "♣":
+                clubs[rank]++;
+                clubs.total++;
+                break;
+            case "♥":
+                hearts[rank]++;
+                hearts.total++;
+                break;
+            case "♠":
+                spades[rank]++;
+                spades.total++;
+                break;
         }
-    }
+    });
+}
 
-    straightFlushChecker() {
-        let suits = [this.clubs, this.diamonds, this.hearts, this.spades];
-        suits.forEach(suit => {
-            if (suit.total > 4) {
-                const consecutiveRanks = this.findConsecutiveRanks(suit);
-                if (consecutiveRanks.length > 4) {
-                    this.printWinner("Straight-Flush", consecutiveRanks);                    
-                    return true;
-                }
+function findWinningHands() {
+    let suits = [clubs, diamonds, hearts, spades];
+    suits.forEach(suit => {           
+        suit.consecutiveRanks = findConsecutiveRanks(suit);
+    });
+    if (straightFlushChecker()) {
+        return;
+    }
+    let suitSameRanks = sameRankCounter();
+    switch(suitSameRanks) {
+        case 4:
+            printWinner("Four-of-a-Kind");
+            return;
+        case 3:
+            if (fullHouseChecker() || flushChecker() || straightChecker()) {
+                return;
+            } else {
+                printWinner("Three-of-a-Kind");
+                return;
             }
-        });
-        return false;
+        case 2:
+            printWinner("Pair");
+            return;
+        default:
+            break;
     }
+    if (flushChecker() || straightChecker()) {
+        return;
+    }
+}
 
-    fourOfAKindChecker() {
-        suits.forEach(suit => {
-            if (suit.total > 4) {
-                const consecutiveRanks = this.findConsecutiveRanks(suit);
-                this.printWinner("Four-Of-A-Kind", consecutiveRanks);
+function straightFlushChecker() {
+    let suits = [clubs, diamonds, hearts, spades];
+    for(let index = 0; index < suits.length; index++) {
+        if (suits[index].total > 4) {
+            if (suits[index].consecutiveRanks.length > 4) {
+                printWinner("Straight-Flush");                    
                 return true;
             }
-        });
-        return false;
-    }
-
-    fullHouseChecker() {
-        let suits = [this.clubs, this.diamonds, this.hearts, this.spades];
-        let threeOfASuit = false;
-        let twoOfASuit = false;
-        suits.forEach(suit => {
-            if (suit.total = 3) {
-                threeOfASuit = true;
-            } else if (suit.total = 2) {
-                twoOfASuit = true;
-            }
-        });
-        if ( threeOfASuit && threeOfASuit) {
-            const consecutiveRanks = this.findConsecutiveRanks(suit);
-            this.printWinner("Full House", consecutiveRanks);
-            return true;               
         }
-        return false;
     }
+    return false;
+}
 
-    findConsecutiveRanks(cards) {
-        let consecutiveRanks = [];
-        let previousRank;        
-        let sortedNumericalRankList = convertRanksToSortedNumericalList(rankList);
-        for (let index = 0; index < sortedNumericalRankList.length; index++) {
-            const rank = sortedNumericalRankList[index];
-            if (rank - 1 == previousRank) {
-                consecutiveRanks.push(rank);
-            }
-            previousRank = rank;            
+function sameRankCounter() {
+    let totalSameRanks = 0;
+    for (let index = 0; index < Object.keys(spades).length - 2; index++) {
+        let suitSameRanks = 0;
+        if (Object.values(spades)[index] > 0) {
+            suitSameRanks++;
+        } if (Object.values(hearts)[index] > 0) {
+            suitSameRanks++;
+        } if (Object.values(clubs)[index] > 0) {
+            suitSameRanks++;
+        } if (Object.values(diamonds)[index] > 0) {
+            suitSameRanks++;
+        } if (suitSameRanks > totalSameRanks) {
+            totalSameRanks = suitSameRanks;
         }
-        consecutiveRanks = this.convertRanksToSortedStringListWithoutDuplicates(consecutiveRanks);
-        return consecutiveRanks;
     }
+    return totalSameRanks;
+}
 
-    convertRanksToSortedNumericalList(cards) {
-        let rankList = [];
-        for (let index = 0; index < Object.keys(cards).length; index++) {
-            let rank;
-            suitKey = Object.keys(cards)[index];
+function fullHouseChecker() {
+    let suits = [clubs, diamonds, hearts, spades];
+    let threeOfASuit = false;
+    let twoOfASuit = false;
+    suits.forEach(suit => {
+        if (suit.total = 3) {
+            threeOfASuit = true;
+        } else if (suit.total = 2) {
+            twoOfASuit = true;
+        }
+    });
+    if ( threeOfASuit && threeOfASuit) {
+        printWinner("Full House");
+        return true;               
+    }
+    return false;
+}
+
+function flushChecker() {
+    let suits = [clubs, diamonds, hearts, spades];
+    suits.forEach(suit => {
+        if (suit.total > 4) {                
+            printWinner("Flush");                    
+            return true;                
+        }
+    });
+    return false;
+}
+
+function straightChecker() {
+    let consecutiveRanks = sortByRank();
+    if (consecutiveRanks.length > 4) {
+        printWinner("Straight");
+        return true;
+    }
+    return false;
+}
+
+function findConsecutiveRanks(cards) {
+    let consecutiveRanks = [];
+    // let previousRank;        
+    let sortedNumericalRankList = convertRanksToSortedNumericalList(cards);
+    for (let index = 0; index < sortedNumericalRankList.length; index++) {
+        if (index == sortedNumericalRankList.length - 1 && parseInt(sortedNumericalRankList[index - 1]) == parseInt(sortedNumericalRankList[index]) - 1) {
+            consecutiveRanks.push(sortedNumericalRankList[index]);
+        }
+        if (parseInt(sortedNumericalRankList[index + 1]) == parseInt(sortedNumericalRankList[index]) - 1) {
+            consecutiveRanks.push(sortedNumericalRankList[index]);
+        } else if (parseInt(sortedNumericalRankList[index]) + 1 == parseInt(sortedNumericalRankList[index - 1])) {
+            consecutiveRanks.push(sortedNumericalRankList[index]);
+        }        
+    }
+    let convertedConsecutiveRanks = convertNumericalRankToSymbol(consecutiveRanks);
+    return convertedConsecutiveRanks;
+}
+
+function convertRanksToSortedNumericalList(cards) {
+    let rankList = [];
+    for (let index = 0; index < Object.keys(cards).length - 2; index++) {
+        let rank;
+        const suitKey = Object.keys(cards)[index];
+        if (cards[suitKey] > 0) {
             switch(suitKey) {
                 case "A":
                     rank = 11;
@@ -126,37 +187,50 @@ export default class App {
             }
             rankList.push(rank);
         }
-        return rankList.sort(function(a, b){return a - b});
     }
-
-    convertRanksToSortedStringListWithoutDuplicates(cards) {
-        let rankList = [];
-        for (let index = 0; index < Object.keys(cards).length; index++) {
-            let rank;
-            let suitKey = Object.keys(cards)[index];
-            switch(suitKey) {
-                case 11:
-                    rank = "A";
-                    break;
-                case 12:
-                    rank = "J";
-                    break;
-                case 13:
-                    rank = "Q";
-                    break;
-                case 14:
-                    rank = "K";
-                    break;
-                default:
-                    rank = suitKey.toString();
-            }
-            rankList.push(rank);
-        }
-        let uniqueRankList = [...new Set(rankList)];
-        return uniqueRankList;
-    }
-
-    printWinner(type, consecutiveRanks) {
-        console.log({type:type,  ranks:consecutiveRanks});
-    }
+    return rankList.sort(function(a, b){return b - a});
 }
+
+function convertNumericalRankToSymbol(ranks) {
+    let convertedRankList = [];
+    ranks.forEach(rank => {
+        let convertedRank;
+        switch(rank) {
+            case 11:
+                convertedRank = "A";
+                break;
+            case 12:
+                convertedRank = "J";
+                break;
+            case 13:
+                convertedRank = "Q";
+                break;
+            case 14:
+                convertedRank = "K";
+                break;
+            default:
+                convertedRank = rank.toString();
+        }
+        convertedRankList.push(convertedRank);
+    });
+    return convertedRankList;
+}
+
+function sortByRank() {
+    let ranks = [];
+    let suits = [clubs, diamonds, hearts, spades];
+    suits.forEach(suit => {
+        let numericalList = convertRanksToSortedNumericalList(suit);
+        ranks = ranks.concat(numericalList);           
+    });
+    ranks.sort(function(a, b){return b - a});
+    return convertNumericalRankToSymbol(ranks);
+}
+
+function printWinner(type) {
+    const ranksList = sortByRank();
+    const ranks = [...new Set(ranksList)];
+    console.log({type:type, ranks});
+}
+
+hand(["A♥", "K♥"], ["J♥", "5♥", "5♠", "5♦", "5♣"]);
